f9fdd7e527c8a12104068a4e8dd1fe7f6f48fb1b
Vaibhav <raivaibhav08@gmail.com>
2019-06-14 16:43:06 +0530

    Add: Helper Message

--- bot.js
+++ bot.js
@@ -33,21 +33,21 @@ facebookController.hears(['.*'],['message_received','facebook_postback'], async 
     });
 
     let askDOB = function(response, convo){
-      
+      convo.say(`Hi ${response.text} ! nice to meet you`);
       convo.ask('Please tell me your birth date in YYYY-MM-DD format', async function(response, convo){
         await createMessage({id:response.mid, user: response.user, text: response.text, timestamp: response.timestamp, question: response.question});
         let days = getDays(response.text);
         if (days){
           askDisplayOld(days, response, convo);
         }else{
+          bot.reply(response, `Those dates don't seem right`);
           convo.repeat();
         }
-        
         convo.next();
       });
     };
     let askDisplayOld = function(days,response, convo){
-      convo.ask('You want to know how old are you in days',
+      convo.ask(`You want to know how old are you in days?\nYes: No`,
       [
         {
           pattern:bot.utterances.yes,
@@ -62,14 +62,12 @@ facebookController.hears(['.*'],['message_received','facebook_postback'], async 
           callback: async function(response,convo){
             await createMessage({id:response.mid, user: response.user, text: response.text, timestamp: response.timestamp, question: response.question});
             convo.say("Good Bye 👋");
-            // convo.repeat();
             convo.next();
           }
         },
         {
           default: true,
           callback: function(response,convo) {
-            // just repeat the question
             convo.repeat();
             convo.next();
           }
