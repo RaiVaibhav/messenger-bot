1e945b9c6aeb556402ef5f4d8ab542887f5dca5d
Vaibhav <raivaibhav08@gmail.com>
2019-06-16 15:34:45 +0530

    Add: Button template
    Give option to user to select yes/no,
    additionally user can respond with text on same.

--- bot.js
+++ bot.js
@@ -19,62 +19,100 @@ var webserver = require('./index.js')(facebookController);
 // facebookController.api.messenger_profile.greeting("Welcome to the Test bot");
 // facebookController.api.messenger_profile.get_started("Get Started");
 
-facebookController.hears(['.*'],['message_received','facebook_postback'], async function(bot, message) {
-  if (message.type==="message_received"){
-    await createMessage({id:message.mid, user: message.user, text: message.text, timestamp: message.timestamp});
-  }
-  bot.startConversation(message, function(err, convo) {
-    
-    convo.say('Hey');
-    convo.ask('Please tell me your full name.', async function(response, convo) {
-      await createMessage({id:response.mid, user: response.user, text: response.text, timestamp: response.timestamp, question:response.question});
-      askDOB(response, convo);
-      convo.next();
-    });
-
-    let askDOB = function(response, convo){
-      convo.say(`Hi ${response.text} ! nice to meet you`);
-      convo.ask('Please tell me your birth date in YYYY-MM-DD format', async function(response, convo){
-        await createMessage({id:response.mid, user: response.user, text: response.text, timestamp: response.timestamp, question: response.question});
-        let days = getDays(response.text);
-        if (days){
-          askDisplayOld(days, response, convo);
-        }else{
-          bot.reply(response, `Those dates don't seem right`);
-          convo.repeat();
-        }
+facebookController.hears(['.*'],['message_received','facebook_postback'],
+  async function(bot, message) {
+    if (message.type==="message_received"){
+      await createMessage(
+        {id:message.mid, user: message.user, text: message.text,
+          timestamp: message.timestamp});
+    }
+    bot.startConversation(message, function(err, convo) {
+      
+      convo.say('Hey');
+      convo.ask('Please tell me your full name.', async function(response, convo) {
+        await createMessage(
+          {id:response.mid, user: response.user, text: response.text,
+            timestamp: response.timestamp, question:response.question});
+        askDOB(response, convo);
         convo.next();
       });
-    };
-    let askDisplayOld = function(days,response, convo){
-      convo.ask(`You want to know how old are you in days?\nYes: No`,
-      [
-        {
-          pattern:bot.utterances.yes,
-          callback:async function(response,convo){
-            await createMessage({id:response.mid, user: response.user, text: response.text, timestamp: response.timestamp, question: response.question});
-            convo.say(`There are ${days} days left until your next birthday`)
-            convo.next()
+
+      let askDOB = function(response, convo){
+        convo.say(`Hi ${response.text} ! nice to meet you`);
+        convo.ask('Please tell me your birth date in YYYY-MM-DD format',
+          async function(response, convo){
+            await createMessage(
+              {id:response.mid, user: response.user, text: response.text,
+                timestamp: response.timestamp, question: response.question});
+            let days = getDays(response.text);
+            if (days){
+              askDisplayOld(days, response, convo);
+            }else{
+              bot.reply(response, `Those dates don't seem right`);
+              convo.repeat();
+            }
+            convo.next();
           }
-        },
-        {
-          pattern:bot.utterances.no,
-          callback: async function(response,convo){
-            await createMessage({id:response.mid, user: response.user, text: response.text, timestamp: response.timestamp, question: response.question});
+        );
+      };
+      let askDisplayOld = function(days,response, convo){
+        let question = "You want to know how old are you in days?";
+        convo.ask({
+          attachment: {
+              'type': 'template',
+              'payload': {
+                  'template_type': 'button',
+                  'text': `${question}`,
+                  'buttons': [
+                      {
+                          'type': 'postback',
+                          'title': 'Yes',
+                          'payload' : 'SAID_YES'
+                      },
+                      {
+                          'type': 'postback',
+                          'title': 'No',
+                          'payload' : 'SAID_NO'
+                      }
+
+                  ]
+              }
+          }
+      },async function (response, convo) {
+          console.log('response', response)
+          if (response.payload==="SAID_NO" && response.type==="facebook_postback"){
+            await createMessage(
+              {id:(""+response.timestamp), user: response.user, text: response.text,
+                timestamp: response.timestamp, question: question});
             convo.say("Good Bye 👋");
-            convo.next();
           }
-        },
-        {
-          default: true,
-          callback: function(response,convo) {
-            convo.repeat();
-            convo.next();
+          if (response.payload==="SAID_YES" && response.type==="facebook_postback"){
+            await createMessage(
+              {id:(""+response.timestamp), user: response.user, text: response.text,
+                timestamp: response.timestamp, question: question});
+            convo.say(`There are ${days} days left until your next birthday`)
           }
-        }
-      ],{},'default');
-    };
-  });
-});
+          if (response.type==="message_received"){
+            if (bot.utterances.yes.test(response.text)){
+              await createMessage(
+                {id:response.mid, user: response.user, text: response.text,
+                  timestamp: response.timestamp, question: response.question});
+              convo.say(`There are ${days} days left until your next birthday`)
+            }else if(bot.utterances.no.test(response.text)){
+              await createMessage(
+                {id:response.mid, user: response.user, text: response.text,
+                  timestamp: response.timestamp, question: response.question});
+              convo.say("Good Bye 👋");
+            }else{
+              bot.reply(response, `Sorry couldn't understand`);
+              convo.repeat();
+            }
+          }
+          convo.next();
+        })
+      };
+    });
+  }
+);
 
 module.exports = webserver;
--- index.js
+++ index.js
@@ -7,40 +7,40 @@ var path = require("path");
 var morgan = require("morgan");
 
 module.exports = function(controller) {
-
-    var webserver = express();
-    if (process.env.NODE_ENV!=="test"){
-      var accessLogStream = fs.createWriteStream(
-        path.join(__dirname, "server.log"),
-        { flags: "a" }
-      );
-      webserver.use(morgan("combined", { stream: accessLogStream }));
+  var webserver = express();
+  if (process.env.NODE_ENV!=="test"){
+    var accessLogStream = fs.createWriteStream(
+      path.join(__dirname, "server.log"),
+      { flags: "a" }
+    );
+    webserver.use(morgan("combined", { stream: accessLogStream }));
+  }
+  webserver.use(bodyParser.json());
+  webserver.use(bodyParser.urlencoded({ extended: true }));
+  webserver.use(express.static('public'));
+  if (process.env.NODE_ENV==="test"){
+    webserver.set('mongo_url', `mongodb://127.0.0.1:27017/test-db`);
+  }else{
+    webserver.set('mongo_url',
+      (process.env.MONGODB_URL || `mongodb://127.0.0.1:27017/messenger`));
+  }
+  mongoose.connect(webserver.get('mongo_url'),{ useNewUrlParser: true },function(err){
+    if (err) {
+      console.error(err);
+      process.exit(1);
     }
-    webserver.use(bodyParser.json());
-    webserver.use(bodyParser.urlencoded({ extended: true }));
-    webserver.use(express.static('public'));
-    if (process.env.NODE_ENV==="test"){
-      webserver.set('mongo_url', `mongodb://127.0.0.1:27017/test-db`);
-    }else{
-      webserver.set('mongo_url', (process.env.MONGODB_URL || `mongodb://127.0.0.1:27017/messenger`));
-    }
-    mongoose.connect(webserver.get('mongo_url'),{ useNewUrlParser: true },function(err){
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      console.log("connected to " + webserver.get('mongo_url'));
-    });
-    require('./routes/message.js')(webserver);
+    console.log("connected to " + webserver.get('mongo_url'));
+  });
+  require('./routes/message.js')(webserver);
 
-    webserver.listen(process.env.PORT || 3000,  null, function() {
-        console.log('Express webserver configured and listening!')
-    });
-    require('./incoming-webhook')(webserver, controller)
-    
-    controller.webserver = webserver;
+  webserver.listen(process.env.PORT || 3000,  null, function() {
+      console.log('Express webserver configured and listening!')
+  });
+  require('./incoming-webhook')(webserver, controller)
+  
+  controller.webserver = webserver;
 
-    return webserver;
+  return webserver;
 
 }
 
